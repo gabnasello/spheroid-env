8b07bd89f2d49a5ab235a5244e8bedbb637f74c0
gabnasello <gabriele.nasello@gmail.com>
2022-05-13 13:07:36 +0200

    manitain cellprofiler venv

--- install_r_packages.R
+++ install_r_packages.R
@@ -2,6 +2,5 @@ options(unzip = 'internal') # not sure it is necessary, put it here after lookin
 options(install.packages.compile.from.source = 'always')
 update.packages(ask=FALSE, repos='https://ftp.gwdg.de/pub/misc/cran/')
 
-install.packages('sp',repos = 'http://cran.us.r-project.org')
 install.packages('psych',repos = 'http://cran.us.r-project.org')
-install.packages('raster',repos = 'http://cran.us.r-project.org')
+install.packages('sp',repos = 'http://cran.us.r-project.org')
